468a9bccd5d2aa0e0759200afe32526681ab8d9b
nikolabulatovic98 <61749921+nikolabulatovic98@users.noreply.github.com>
2020-05-16 01:58:36 +0200

    implementacija
    Implementacija

---
+++ faza6/implementacija/vpnb/app/Models/KorisnikModel.php
@@ -0,0 +1,26 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class KorisnikModel extends Model
+{
+        protected $table      = 'korisnik';
+        protected $primaryKey = 'Username';
+        protected $returnType = 'object';
+        protected $allowedFields = ['Username', 'Pol', 'Password', 'Ime','Prezime'];
+        
+        
+        
+        
+        /*
+        public function pretraga($tekst) {
+            return $this->like('naslov', $tekst)
+                    ->orLike('sadrzaj', $tekst)->findAll();      
+        }
+        
+        public function dohvatiVestiAutora($autor) {
+            return $this->where('autor', $autor)->findAll();      
+        }
+         
+         */
+}
