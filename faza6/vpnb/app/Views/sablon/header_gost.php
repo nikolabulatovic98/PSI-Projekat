6bc883ad7533565bf59505f01d15a68c970c5a62
nikolabulatovic98 <61749921+nikolabulatovic98@users.noreply.github.com>
2020-05-16 02:12:14 +0200

    implementacija
    Implementacija

---
+++ faza6/vpnb/app/Views/sablon/header_gost.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>ETF Vesti</title>
+         <link rel="stylesheet" href="\css\design3.css">
+    </head>
+    <body>
+        <div class="navbar">
+           
+       
+        <?= anchor("Gost/login", "Uloguj se") ?>
+        <?= anchor("Gost/kreiranje", "Kreiraj nalog") ?> 
+        <?= anchor("Gost/kreiranje", "Traži putovanje") ?> 
+            
+        
+      </div>
+        
+        
